f887a04d447189423c329388d64342034db8a1f5
unknown <aijourneybd.blog@gamil.com>
2025-05-23 12:06:41 +0600

    version 1.0

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+const compression = require('compression')
+
+
+
+//middleware
+app.use(express.json())
+app.use(compression())
+app.use(cors())
+
+
+
+const dataPath = path.join(__dirname,'data','post.json')
+
+//routes
+
+app.get('/', (req,res)=>{
+    res.send("This is Home!")
+})
+
+app.get('/api/posts',(req,res)=>{
+    res.setHeader('Content-Type','application/json');
+    const readStream = fs.createReadStream(dataPath)
+    readStream.pipe(res);
+        readStream.on('error', (err) => {
+        console.error("Stream error:", err);
+        res.status(500).send("Server Error");
+    });
+})
+
+
+app.get('/api/posts/:id', (req,res)=>{
+    const ID = Number(req.params.id)
+    fs.readFile(dataPath, 'utf8', (err,data)=>{
+        if (err) throw err;
+        try{
+            const posts = JSON.parse(data)
+            const post = posts.find(post => post.postId === ID)
+            res.json(post)
+        }catch(err){
+            res.send(err)
+        }
+    })
+})
+
+
+app.listen(3000, ()=>{
+    console.log("Server is Running!")
+})
